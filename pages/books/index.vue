69109e13871517b244c14fc9c1900d7e7f1d84d7
Nguyen Van Nguyen <n.v.nguyen@azoom.jp>
2020-10-28 17:41:44 +0700

    first commit

---
+++ pages/books/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>test</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  created() {
+    axios
+      .post("/books", {
+        bookName: "Fred",
+        bookLastName: "Flintstone",
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+};
+</script>
